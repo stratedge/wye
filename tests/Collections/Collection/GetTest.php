bb4cdb961ea6d237c64a712b5c1ef10df9ff9b23
xjstratedgebx <jarret@solvomedia.com>
2017-05-14 20:52:28 -0400

    Create new generic Collection class

---
+++ tests/Collections/Collection/GetTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Collections\Collection;
+
+use InvalidArgumentException;
+use Stratedge\Wye\Collections\Collection;
+use Stratedge\Wye\Wye;
+
+class GetTest extends \Tests\TestCase
+{
+    public function testNotSetReturnsNull()
+    {
+        $collection = new Collection(new Wye, [1, 2, 3]);
+
+        $this->assertNull($collection->get(4));
+    }
+
+    public function testNotSetReturnsDefault()
+    {
+        $collection = new Collection(new Wye, [1, 2, 3]);
+
+        $this->assertSame('Engine', $collection->get(4, 'Engine'));
+    }
+
+    public function testReturnsValueAtKey()
+    {
+        $collection = new Collection(new Wye, [1, 2, 3]);
+
+        $this->assertSame(2, $collection->get(1));
+
+        $collection['Ladder'] = 'Rescue';
+
+        $this->assertSame('Rescue', $collection->get('Ladder'));
+    }
+}

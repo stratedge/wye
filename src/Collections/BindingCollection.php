53dd9ab7cfc40ccb48e1aebbad5254890311fcd1
xjstratedgebx <jarret@solvomedia.com>
2017-05-18 18:34:45 -0400

    Add BindingCollection::hydrateFromArray()

--- src/Collections/BindingCollection.php
+++ src/Collections/BindingCollection.php
@@ -17,4 +17,34 @@ class BindingCollection extends Collection
     {
         parent::__construct($wye, $items, Binding::class);
     }
+
+    /**
+     * Rebuilds the contents of the collection based on the provided array. Keys
+     * become binding parameters and values are used for binding values.
+     * 0-indexed numerical-keyed arrays are shifted once to begin with 1.
+     *
+     * @param  array  $array
+     * @return self
+     */
+    public function hydrateFromArray(array $array)
+    {
+        $this->clear();
+
+        //If this is a 0-indexed numerical key array, add 1 to all the keys so
+        //it is a 1-indexed numerical key array
+        if (current(array_keys($array)) === 0) {
+            $array = array_combine(
+                range(1, count($array)),
+                array_values($array)
+            );
+        }
+
+        foreach ($array as $key => $value) {
+            $this->push(
+                $this->wye->makeBinding($key, $value)
+            );
+        }
+
+        return $this;
+    }
 }
